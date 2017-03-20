7896d651391e7dc16273b2482a18b609ab9c6752
Janis Kacens <janis_kacens@hotmail.com>
2017-03-20 21:20:50 +0000

    2.1 uzdevums

---
+++ Plusmas/Plusmas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Plusmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thr = new Thread(Go);
+            thr.Start();
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine("T1");
+            }
+        }
+
+        public static void Go()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine("T1");
+            }
+        }
+    }
+}
