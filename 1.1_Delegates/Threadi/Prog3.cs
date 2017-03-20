56524e384f23c17b6420a3c4123f01ff3a2c9bf7
Janis Kacens <janis_kacens@hotmail.com>
2017-03-20 19:07:40 +0000

    4. uzd Threadi

---
+++ 1.1_Delegates/Threadi/Prog3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threadi
+{
+    class Prog3
+    {
+        static void Main()
+        {
+            Thread t = new Thread(Go);
+            t.Start();
+            t.Join();
+            Console.WriteLine("Thread t has ended!");
+
+        }
+
+        static void Go()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("y");
+        }
+    }
+}
---
+++ 1.1_Delegates/Threadi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threadi
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Thread t = new Thread(Go);
+        //    t.Start();
+
+        //    for (int i = 0; i < 1000; i++)
+        //    {
+        //        Console.WriteLine("x");
+        //    }
+
+        //}
+
+        static void Go()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("\ty");
+            }
+        }
+
+
+    }
+}
